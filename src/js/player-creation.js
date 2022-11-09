7aa8cda1c0a6fcd138ccc2cdc9185e4187215711
Brian Noh <noh24@ymail.com>
2022-11-09 17:35:30 -0500

    add code to clear test to remove item from inventory
    Co-authored-by: Alex Johnson <alex.johnson293@gmail.com>

--- src/js/player-creation.js
+++ src/js/player-creation.js
@@ -23,12 +23,15 @@ export class PlayerCreation {
   }
   
   addToInventory(item) {
-    if (this.inventory.length >=5) {
+    if (this.inventory.length >= 5) {
       return "too much stuff";
     } else {
       this.inventory.push(item);
     }
   }
+
+  removeItem(item) {
+    let index = this.inventory.indexOf(item);
+    this.inventory.splice(index, 1);
+  }
 }
-
-
