53e2d72ebc28299e5fa7329082e2f55a8ee49b4d
Brian Noh <noh24@ymail.com>
2022-11-09 14:32:19 -0500

    refactor tests

--- __tests__/player-creation.test.js
+++ __tests__/player-creation.test.js
@@ -13,20 +13,23 @@ describe('playerCreation', () => {
   test('call, create object with properties: name, archetype, basestats',  () => {
     let name = "Alex";
     let archetype = "Goliath";
-    let baseStats = [3, 5, 7, 3, 10];
-    let player = new PlayerCreation(name, archetype,);
+    let player = new PlayerCreation(name, archetype);
     expect(player.playerName).toEqual(name);
     expect(player.archetype).toEqual(archetype);
-    expect(player.baseStats).toEqual(baseStats);
+    expect(player.baseStats).toEqual([]);
   });
 
   test('monster base stats are assigned based on their archetype choice', () => {
     let archetype = "Goliath";
+    let archetype2 = "Human";
     let player = new PlayerCreation(archetype);
+    let player2 = new PlayerCreation(archetype2);
     player.assignBaseStats(archetype);
+    player2.assignBaseStats(archetype2);
     expect(player.baseStats).toEqual([3, 5, 7, 3, 10]);
+    expect(player2.baseStats).toEqual([0, 0, 0, 0, 0]);
   });
-
+  
 //reference:
 //lo 3, med 5, hi 7, exp, 10
 // tanky: goliath
