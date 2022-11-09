53ae158ea13b0b04c6c2d60e61f7a80282b24a51
Brian Noh <noh24@ymail.com>
2022-11-09 14:29:08 -0500

    Co-authored-by: alexiusvdt <alexiusvdt@users.noreply.github.com>

--- __tests__/example-for-export.test.js
+++
@@ -1,9 +0,0 @@
-// import Whatever from ./path
-
-// describe('whatever whatever', () => {
-  
-//   test('should correctly test for whatever you want', () => {
-//     let code = whatever();
-//     expect(whatever()).toEqual('whatever');
-//   });
-// });
