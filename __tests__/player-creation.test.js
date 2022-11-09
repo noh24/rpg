2916ec9f8ec1c6e5f85fd81e2b7d998fba524d6a
Brian Noh <noh24@ymail.com>
2022-11-09 18:02:48 -0500

    refactor initializeCharacter test
    Co-authored-by: Alex Johnson <alex.johnson293@gmail.com>

--- __tests__/player-creation.test.js
+++ __tests__/player-creation.test.js
@@ -1,4 +1,4 @@
-import { PlayerCreation } from './../src/js/player-creation';
+import PlayerCreation from './../src/js/player-creation';
 
 describe('playerCreation', () => {
 
@@ -12,66 +12,32 @@ describe('playerCreation', () => {
     expect(player.inventory).toEqual([]);
   });
 
-  test('starting base stats and item are assigned based on their choice of "goliath"', () => {
+  test('base stats are assigned based on their choice of "goliath"', () => {
     let archetype = "Goliath";
     let player = new PlayerCreation(archetype);
     player.initializeCharacter(archetype);
     expect(player.baseStats).toEqual([3, 5, 10, 3, 7]);
-    expect(player.inventory).toEqual(["brass knuckles"]);
   });
   
-  test('starting base stats and item are assigned based on their choice of "Slasher"', () => {
+  test('base stats and item are assigned based on their choice of "Slasher"', () => {
     let archetype = "Slasher";
     let player = new PlayerCreation(archetype);
     player.initializeCharacter(archetype);
     expect(player.baseStats).toEqual([7, 10, 5, 3, 3]);
-    expect(player.inventory).toEqual(["chef knives"]);
   });
   
-  test('starting base stats and item are assigned based on their choice of "Mystic"', () => {
+  test('base stats and item are assigned based on their choice of "Mystic"', () => {
     let archetype = "Mystic";
     let player = new PlayerCreation(archetype);
     player.initializeCharacter(archetype);
     expect(player.baseStats).toEqual([5, 3, 3, 10, 7]);
-    expect(player.inventory).toEqual(["orb"]);
   });
   
-  test('starting base stats and item are assigned based on their choice of "Brainiac"', () => {
+  test('base stats and item are assigned based on their choice of "Brainiac"', () => {
     let archetype = "Brainiac";
     let player = new PlayerCreation(archetype);
     player.initializeCharacter(archetype);
     expect(player.baseStats).toEqual([5, 5, 3, 7, 10]);
-    expect(player.inventory).toEqual(["glock"]);
   });
-  
-  test('should add item to player\'s inventory ', () => {
-    let archetype = "Brainiac";
-    let item = "chest-equipment"
-    let player = new PlayerCreation(archetype);
-    player.initializeCharacter(archetype);
-    player.addToInventory(item);
-    expect(player.inventory).toEqual(["glock", "chest-equipment"]);
-  });
-  
-  test('prevent adding to inventory that holds more than 5 items', () => {
-    let archetype = "Brainiac";
-    let item = "fail-item";
-    let player = new PlayerCreation(archetype);
-    player.inventory = ["glock", "two", "three", "four", "five"];
-    player.addToInventory(item);
-    expect(player.inventory).toEqual(["glock", "two", "three", "four", "five"]);
-    expect(player.addToInventory(item)).toEqual("too much stuff");
-  });
-
-  test('will remove specified item from inventory', () => {
-    let archetype = "Goliath"
-    let player = new PlayerCreation(archetype);
-    player.inventory = ["glock", "two", "three", "four", "five"];
-    player.removeItem("glock");
-    expect(player.inventory).toEqual(["two", "three", "four", "five"]);
-
-  
-
-  })
 });
 
